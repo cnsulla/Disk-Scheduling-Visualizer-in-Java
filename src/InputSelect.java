35a280dbf22067e000bb00152b7cb8c2b378b7a0
cnsulla <cnsulla@gmail.com>
2022-05-24 16:17:52 +0800

    added splash and input screen bgs

--- src/InputSelect.java
+++ src/InputSelect.java
@@ -1,5 +1,7 @@
 import javax.swing.JPanel;
 import javax.swing.filechooser.FileNameExtensionFilter;
+
+import javax.imageio.ImageIO;
 import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JFileChooser;
@@ -7,6 +9,13 @@ import javax.swing.JOptionPane;
 
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import java.awt.image.BufferedImage;
+import java.awt.Color;
+import java.awt.Graphics;
+
+import java.io.File;
+import java.io.IOException;
+
 class InputSelect extends JPanel{
     private static int BUTTON_HGAP = 50;
 
@@ -14,8 +23,8 @@ class InputSelect extends JPanel{
     private JButton randomInput;
     private JButton textInput;
     private JButton userInput;
-
     private JButton back;
+    private BufferedImage img;
 
     InputSelect(Controller controller){
         this.controller = controller;
@@ -24,6 +33,8 @@ class InputSelect extends JPanel{
     }
     private void initComponents(){
         randomInput = new JButton("Random");
+        randomInput.setBackground(Color.decode("#17256f"));
+        randomInput.setForeground(Color.WHITE);
         randomInput.addActionListener(new ActionListener(){
             @Override
             public void actionPerformed(ActionEvent evt){
@@ -31,7 +42,10 @@ class InputSelect extends JPanel{
                controller.show(Controller.SIMULATION_SCREEN);
             }
         });
+
         textInput = new JButton("Text-File");
+        textInput.setBackground(Color.decode("#17256f"));
+        textInput.setForeground(Color.WHITE);
         textInput.addActionListener(new ActionListener(){
             @Override
             public void actionPerformed(ActionEvent evt){
@@ -51,7 +65,10 @@ class InputSelect extends JPanel{
                 }
             }
         });
+
         userInput = new JButton("User Input");   
+        userInput.setBackground(Color.decode("#17256f"));
+        userInput.setForeground(Color.WHITE);
         userInput.addActionListener(new ActionListener(){
             @Override
             public void actionPerformed(ActionEvent evt){
@@ -60,6 +77,8 @@ class InputSelect extends JPanel{
         });  
 
         back = new JButton("Back");
+        back.setBackground(Color.decode("#17256f"));
+        back.setForeground(Color.WHITE);
         back.addActionListener(new ActionListener(){
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -75,14 +94,31 @@ class InputSelect extends JPanel{
         buttonPanel.add(randomInput);
         buttonPanel.add(textInput);
         buttonPanel.add(userInput);
+        buttonPanel.setBackground(Color.decode("#17256f"));
 
 
-        JPanel centeringPanel = new JPanel();
+        JPanel centeringPanel = new JPanel() {
+            @Override
+            protected void paintComponent(Graphics g) {
+                super.paintComponent(g);
+                g.drawImage(img, 0, 0, null);
+            }
+        };
         centeringPanel.setBorder(BorderFactory.createEmptyBorder(controller.getHeight()/2,0,0,0));
         centeringPanel.add(buttonPanel);
+        try {
+            img = ImageIO.read(new File(System.getProperty("user.dir") + "/src/images/inputscreen.png"));
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        centeringPanel.setBackground(Color.decode("#17256f"));
+        // centeringPanel.setSize(800, 500);
+        centeringPanel.repaint();
 
         JPanel southPanel = new JPanel();
         southPanel.setLayout(new java.awt.BorderLayout());
+        southPanel.setBackground(Color.decode("#17256f"));
         southPanel.add(back, java.awt.BorderLayout.WEST);
         add(centeringPanel, java.awt.BorderLayout.CENTER);
         add(southPanel, java.awt.BorderLayout.SOUTH);
--- src/MainMenu.java
+++ src/MainMenu.java
@@ -1,7 +1,7 @@
 import javax.swing.JPanel;
+import javax.swing.SwingConstants;
 import javax.swing.BorderFactory;
 import javax.swing.JButton;
-import javax.swing.ImageIcon;
 import javax.swing.JLabel;
 import javax.imageio.ImageIO;
 import java.awt.event.ActionListener;
@@ -10,8 +10,8 @@ import java.awt.image.BufferedImage;
 import java.awt.Dimension;
 import java.awt.GridLayout;
 import java.awt.BorderLayout;
-import java.awt.Image;
 import java.awt.Color;
+import java.awt.Graphics;
 import java.io.File;
 import java.io.IOException;
 
@@ -23,6 +23,7 @@ class MainMenu extends JPanel{
     private JButton start;
     private JButton exit;
     private JLabel label1;
+    private BufferedImage img;
 
     MainMenu(Controller controller){
         this.controller = controller;
@@ -31,24 +32,12 @@ class MainMenu extends JPanel{
     }
 
     private void initComponents(){
-
         
-        // label1 = new JLabel();
-        // label1.setPreferredSize(new Dimension(250, 100));
-
-        // BufferedImage img = null;
-        // try {
-        //     img = ImageIO.read(new File("src/images/test.png"));
-        // } catch (IOException e) {
-        //     e.printStackTrace();
-        // }
-        
-        // Image dimg = img.getScaledInstance(label1.getWidth(), label1.getHeight(), Image.SCALE_SMOOTH);
-        // ImageIcon icon = new ImageIcon (dimg);
-        // label1.setIcon(icon);
+        label1 = new JLabel("Disk Scheduling Visualizer", SwingConstants.CENTER);
+        label1.setPreferredSize(new Dimension(200, 45));
 
         start = new JButton("Start");
-        start.setBackground(Color.GRAY);
+        start.setBackground(Color.decode("#17256f"));
         start.setForeground(Color.WHITE);
         start.addActionListener(new ActionListener(){
             @Override
@@ -58,7 +47,7 @@ class MainMenu extends JPanel{
         });
 
         exit = new JButton("Exit");
-        exit.setBackground(Color.GRAY);
+        exit.setBackground(Color.decode("#17256f"));
         exit.setForeground(Color.WHITE);
         exit.addActionListener(new ActionListener(){
             @Override
@@ -67,19 +56,37 @@ class MainMenu extends JPanel{
             }
         });
     }
+
     private void initPanels(){        
         setLayout(new BorderLayout());
 
         JPanel buttonPanel = new JPanel();
-        buttonPanel.setLayout(new GridLayout(2,0,0,BUTTON_VGAP));
+        buttonPanel.setLayout(new GridLayout(2,1,0,BUTTON_VGAP));
+        buttonPanel.setBackground(Color.decode("#17256f"));
+        
         buttonPanel.add(start);
         buttonPanel.add(exit);
         
-        JPanel centeringPanel = new JPanel();
-        centeringPanel.setBorder(BorderFactory.createEmptyBorder(controller.getHeight()/2,0,0,0));
+        JPanel centeringPanel = new JPanel() {
+            @Override
+            protected void paintComponent(Graphics g) {
+                super.paintComponent(g);
+                g.drawImage(img, 0, 0, null);
+            }
+        };
+        centeringPanel.setBorder(BorderFactory.createEmptyBorder((controller.getHeight()/2),0,0,0));  
+        centeringPanel.setBackground(Color.BLUE);
+        try {
+            img = ImageIO.read(new File(System.getProperty("user.dir") + "/src/images/splash.png"));
 
-        centeringPanel.add(buttonPanel);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        // centeringPanel.setSize(800, 500);
+        centeringPanel.repaint();
+
         // centeringPanel.add(label1);
+        centeringPanel.add(buttonPanel);
         add(centeringPanel, BorderLayout.CENTER);
     }
 }
